eec05f62d8614dbaf367b7d2d38788aaedcd9dab
Michael <mstyvejr@gmail.com>
2014-03-21 00:24:05 -0500

    AND WERE BACK IN TIME FOR TEA

--- mp3/oneInstr.asm
+++ mp3/oneInstr.asm
@@ -1,25 +1,16 @@
 ORIGIN
 SEGMENT  CodeSegment:
-   LDR  R1, R0, ONE		; R1 <= 1
-   NOP
-   NOP
-   NOP
-   NOP
-   NOP
-   LDR R2, R0, TWO      ; R2 <= 2
-   NOP
-   NOP
-   NOP
-   NOP
-   NOP
-   STR R1, R0, TWO       ; R3 <= 1+2
-   NOP
-   NOP
-   NOP
-   NOP
-   NOP
-   NOP
-   ONE:	DATA2 4xFFFF
-   TWO: DATA2 4x0002
+    LDR R1, R0, ONE
+    NOP
+    NOP
+    NOP
+    NOP
+YOLO:
+    BRnzp YOLO
+    NOP
+    NOP
+    NOP
+    NOP
+    ONE: DATA2 4x0001
    
    
