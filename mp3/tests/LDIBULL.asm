35fd6c3510ab4ae35b6652d3fb55519bcff78f0a
Caleb Freed <freed2@linux-a3.ews.illinois.edu>
2014-04-18 12:21:02 -0500

    added missing files

---
+++ mp3/tests/LDIBULL.asm
@@ -0,0 +1,39 @@
+SEGMENT BOOT:
+    LEA R1, BULLSHIT
+    NOP
+    NOP
+    NOP
+    NOP
+    ;ADD R1, R1, 1
+    LEA R3, COW0
+    NOP
+    NOP
+    NOP
+    NOP
+    STR R3, R1, BULL10
+    ;LDB R2, R1, BULL10 ;Loads 0011 into R2
+    LDI R4, R1, BULL10
+    ADD R2, R4, 1 ;Makes r2 0100
+    ADD R5, R2, 3
+    NOP
+    NOP
+    NOP
+    NOP
+    STI R5, R1, BULL10
+    NOP
+    NOP
+    NOP
+
+    
+SEGMENT BULLSHIT:
+BULL0:    DATA2 4X0001 ;0
+BULL2:    DATA2 4x0101 ;2
+BULL4:    DATA2 4x1010 ;4
+BULL6:    DATA2 4x1111 ;6
+BULL8:    DATA2 4x0000 ;8
+BULL10:   DATA2 4xAAAA ;10 3A
+
+SEGMENT HORSE:
+COW0    : DATA2 4x8008 ; line 60
+SHIT2   : DATA2 4x4004 ;
+    
